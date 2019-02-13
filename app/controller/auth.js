4882147295749e1830e55c78928fe665ee1a7f9a
祝传鹏 <luomusha@gmail.com>
2019-02-13 15:09:47 +0800

    egg-init

--- app/controller/auth.js
+++ app/controller/auth.js
@@ -1,46 +1,63 @@
 'use strict'
 
 const Controller = require('egg').Controller
+const OAuth2Server = require('oauth2-server')
 
 class AuthController extends Controller {
 
   checkAuthorizeParam (client, query) {
-
+    if (query.redirect_uri && !client.redirectUris.includes(query.redirect_uri)) {
+      throw new OAuth2Server.InvalidRequestError()
+    }
+    if (!query.redirect_uri) {
+      query.redirect_uri = client.redirectUris[0]
+    }
   }
 
-  async authorize () {
+  async renderLoginOrAuthorize () {
     const { ctx, service } = this
     const userId = ctx.session.userId
     try {
-
       if (userId) {
-        console.log('有session', ctx.session, ctx.request.query)
         const requestQueryParam = { ...ctx.request.query }
         const user = await service.user.getUserById(userId)
         const client = await service.client.getClientById(requestQueryParam.client_id)
         this.checkAuthorizeParam(client, requestQueryParam)
         await ctx.render('auth/authorize', { user, client, query: requestQueryParam })
     } else {
-        console.log('没session', ctx.session)
         await ctx.render('auth/login')
     }
     } catch (e) {
-      await ctx.render('error')
+      await ctx.render('error', e)
     }
   }
 
-  async session () {
+  async loginSession () {
     const { ctx } = this
     const { User } = this.app.model
     const { username, password } = ctx.request.body
     try {
       const user = await User.findOne({ username, password })
-      ctx.session.userId = user._id
+      if (user) {
+        ctx.session.userId = user._id
+      }
+      ctx.redirect(ctx.request.header.referer)
+    } catch (e) {
+      console.log('eeee', e)
+      await ctx.render('error', e)
+    }
+  }
+
+  async logoutSession () {
+    const { ctx } = this
+    try {
+      ctx.session.userId = null
       ctx.redirect(ctx.request.header.referer)
     } catch (e) {
       await ctx.render('error', e)
     }
   }
+
 }
 
 module.exports = AuthController
--- app/router.js
+++ app/router.js
@@ -8,8 +8,9 @@ module.exports = app => {
   const oauth2 = app.oauth2
 
   // oauth2专用接口
-  router.get('/oauth2/authorize', 'auth.authorize')
-  router.post('/oauth2/session', 'auth.session')
+  router.get('/oauth2/authorize', 'auth.renderLoginOrAuthorize')
+  router.post('/oauth2/login', 'auth.loginSession')
+  router.post('/oauth2/logout', 'auth.logoutSession')
   router.post('/oauth2/authorize', oauth2.authorize())
   router.post('/oauth2/token', oauth2.token())
   router.get('/oauth2/authenticate', oauth2.authenticate())
